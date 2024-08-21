40941e6f39288b48318ac55f7697bc8c7440f39a
DropoutCoder-true <vighneshzagade4@gmail.com>
2024-08-21 12:36:33 +0530

    added-css-and-fetched-an-omdbapi

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
-import { useState, useEffect } from "react";
+import React, { useEffect } from "react"; 
+import "./App.css"
+import SearchIcon from "./search.svg"; 
 
-function App() {
-  const [count, setCount] = useState(0);
+// API Key:- 6171b493
+// API:- http://www.omdbapi.com/?i=tt3896198&apikey=6171b493
 
-  useEffect(() => {
-    setCount(100); 
-  }, [])
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=6171b493";
 
-  return (
-    <div className="App">
-      <button onClick={() => setCount(prevCount => prevCount + 1)}> + </button>
-      <h1>
-        {count}
-      </h1>
-      <button onClick={() => setCount(prevCount => prevCount - 1)}> - </button>
-    </div>
-  );
+const movie1 = {
+    "Title": "Italian Spiderman",
+    "Year": "2007",
+    "imdbID": "tt2705436",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BZWQxMjcwNjItZjI0ZC00ZTc4LWIwMzItM2Q0YTZhNzI3NzdlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_SX300.jpg"
 }
 
-export default App;
+const App = () => {
+    const searchMovies = async (title) => {
+        const resp = await fetch(`${API_URL}&s=${title}`)
+        const data = await resp.json(); 
+        console.log(data.Search); 
+    }
+
+    useEffect(() => {  
+        searchMovies("Spiderman"); 
+    }, []) 
+
+    return(
+        <div className="app">
+            <h1>MovieLand</h1>
+
+            <div className="search">
+                <input placeholder="Search For Movies" value="Superman" onChange={() => {}}/>    
+                <img src={SearchIcon} onClick={() => {}} alt={"Search-icon-image"}/>
+            </div>
+
+            <div className="container">
+
+            </div>
+        </div>
+    )
+}
+
+export default App; 
